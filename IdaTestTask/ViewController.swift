14f5af3b082feba12ed6bb88ef4144ae33106ec6
0atme0 <0atme0@gmail.com>
2016-12-27 19:14:51 +0300

    Initial Commit

---
+++ IdaTestTask/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IdaTestTask
+//
+//  Created by Andrey Ildyakov on 27.12.16.
+//  Copyright © 2016 Andrey Ildyakov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
